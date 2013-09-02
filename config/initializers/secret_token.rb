053fda6e9415888fd2e416a5054a7d2187b42ffc
Ryan Rich <rrichrs@gmail.com>
2013-09-02 14:43:43 -0400

    inital

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeneralistIo::Application.config.secret_key_base = '9304e82c7dda3b846af1bfdb9cc46e4520573cb52698aa418050f1ac65838a0a32f5bd468b71e601b12d99df2c1836603324026e3df5ef5fb458879e23d01b74'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeneralistIo::Application.config.session_store :cookie_store, key: '_generalist_io_session'
